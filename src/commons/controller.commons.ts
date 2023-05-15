a7cf91e2d79880221ee6a5b277030f1bdc1ccbfd
Jesus-0sorio <jaoz10.16@gmail.com>
2023-05-14 23:15:50 -0500

    add all methods services commons

--- src/commons/controller.commons.ts
+++ src/commons/controller.commons.ts
@@ -9,31 +9,37 @@ import {
   Post,
 } from '@nestjs/common';
 import { BaseService } from './service.commons';
-import { Entity } from 'typeorm';
 
 export abstract class BaseController<T> {
   abstract getService(): BaseService<T>;
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  async save(@Body() entity: T): Promise<T> {
-    return await this.getService().save(entity);
-  }
+
   @Get()
-  async findAll(): Promise<T> {
+  @HttpCode(HttpStatus.OK)
+  async findAll() {
     return await this.getService().findAll();
   }
+
   @Get(':id')
-  async findOne(@Param('id') id: Promise<T>) {
-    return await this.getService().findOne(id);
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id') id: string) {
+    return await this.getService().findOne(+id);
   }
 
-  @Post(':id')
-  @HttpCode(HttpStatus.OK)
-  async delete(@Param('id') id: any) {
-    return this.getService().delete(id);
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async save(@Body() entity: T) {
+    return await this.getService().create(entity);
   }
+
   @Patch(':id')
-  async update(@Param('id') id: Promise<T>, @Body() updateDto: any) {
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async update(@Param('id') id: string, @Body() updateDto: any) {
     return await this.getService().update(+id, updateDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string) {
+    return this.getService().delete(+id);
   }
 }
--- src/commons/service.commons.ts
+++ src/commons/service.commons.ts
@@ -1,25 +1,66 @@
-import { Repository } from 'typeorm';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { FindOptionsWhere, Repository } from 'typeorm';
 
 export abstract class BaseService<T> {
-  update(arg0: number, updateDto: any) {
-    throw new Error('Method not implemented.');
-  }
-  delete(id: any) {
-    throw new Error('Method not implemented.');
-  }
-  findOne(id: Promise<T>) {
-    throw new Error('Method not implemented.');
-  }
-  findAll(): T | PromiseLike<T> {
-    throw new Error('Method not implemented.');
-  }
   abstract getRepository(): Repository<T>;
 
-  save(entity: T): Promise<T> {
+  create(entity: T) {
     try {
       return this.getRepository().save(entity);
     } catch (error) {
+      throw new HttpException(error.message, error.status);
+    }
+  }
+
+  async findOne(id: any): Promise<T> {
+    try {
+      return await this.getRepository().findOne({
+        where: { id: id },
+      } as FindOptionsWhere<any>);
+    } catch (error) {
       console.log(error);
+      throw new HttpException(error.message, 500);
+    }
+  }
+  async findAll() {
+    try {
+      const result = this.getRepository().find();
+      const data = await result;
+      for (const item of data) {
+        delete item['password'];
+      }
+      return data;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async update(id: number, updateDto: any) {
+    try {
+      if (
+        await this.getRepository().findOne({
+          where: { id: id },
+        } as FindOptionsWhere<any>)
+      ) {
+        return this.getRepository().update(id, updateDto);
+      }
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  async delete(id: number) {
+    try {
+      if (
+        await this.getRepository().findOne({
+          where: { id: id },
+        } as FindOptionsWhere<any>)
+      ) {
+        return this.getRepository().delete(id);
+      }
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
